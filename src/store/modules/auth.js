552a8edddc02a092348753739261b2ed4aeab0be
Peaches <shinasaki07@gmail.com>
2018-04-11 23:46:11 +0700

    :tada: pepare to codeing

---
+++ src/store/modules/auth.js
@@ -0,0 +1,19 @@
+// initial state
+const state = {};
+
+// getters
+const getters = {};
+
+// actions
+const actions = {};
+
+// mutations
+const mutations = {};
+
+// export
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+};
