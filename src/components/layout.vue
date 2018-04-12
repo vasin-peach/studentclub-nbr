54953dfc33b7eba04a8d9cbe5a1e5122fcc55f15
Peaches <shinasaki07@gmail.com>
2018-04-12 17:32:12 +0700

    :construction: setting route

---
+++ src/components/layout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="layout">
+    Layout
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'layout'
+};
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import layout from '@component/layout';
 
 Vue.use(Router);
 
 export default new Router({
-  routes: []
+  routes: [{ path: '/', component: layout, name: layout }]
 });
