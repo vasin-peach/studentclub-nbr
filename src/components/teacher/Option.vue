00e125dc1c59936b223215554e6109a0b6bd5648
Peaches <shinasaki07@gmail.com>
2018-05-20 00:57:23 +0700

    :iphone: add backward in option page

--- src/components/teacher/Option.vue
+++ src/components/teacher/Option.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="teacher-option">
+
+    <!-- Button Link Main -->
+    <router-link :to="{ name: 'Student_Club'}" class="main-button-link" v-if="user.permission >= 2">
+      <i class="fas fa-chevron-circle-left"></i>
+      <div class="button-text">หน้าหลัก</div>
+    </router-link>
+
     <b-row class="teacher-option-body m-0">
       <b-col cols="12" class="text-center">
         <hr> ปิดเปิดการลงทะเบียนชุมนุมของนักเรียน
